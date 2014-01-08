fcb71f4787208d96778fe31f97a46b977aae7ba5
Manuel Bahamondez Honores <manuel@bahamondez.com>
2014-01-08 17:17:18 -0300

    wkhtmltopdf

--- conectaideas/recipes/default.rb
+++ conectaideas/recipes/default.rb
@@ -9,20 +9,20 @@
 include_recipe 'apt'
 include_recipe 'cron'
 
-apt_repository "postgres" do
-  uri "http://apt.postgresql.org/pub/repos/apt/"
-  distribution "precise-pgdg"
-  key "https://www.postgresql.org/media/keys/ACCC4CF8.asc"
-  components ["main"]
+apt_repository 'postgres' do
+  uri 'http://apt.postgresql.org/pub/repos/apt/'
+  distribution 'precise-pgdg'
+  key 'https://www.postgresql.org/media/keys/ACCC4CF8.asc'
+  components ['main']
 end
 
 package 'libpq-dev'
-package "git-core"
-package "libxml2"
-package "libxslt-dev"
-package "libmagickwand-dev"
-package "postgresql-client-9.3"
-package "monit"
+package 'git-core'
+package 'libxml2'
+package 'libxslt-dev'
+package 'libmagickwand-dev'
+package 'postgresql-client-9.3'
+package 'monit'
 package 'imagemagick'
 package 'wkhtmltopdf'
 
@@ -31,30 +31,24 @@ template "/etc/environment" do
   mode 0555
 end
 
-node[:deploy].each do |application, deploy|
-  template "/etc/monit/conf.d/sidekiq_#{application}.monitrc" do
-    owner 'root'
-    group 'root'
-    mode 0644
-    source "monitrc.conf.erb"
-    variables({
-      :worker_count => 1,
-      :app_name => application,
-      :deploy => deploy
-    })
-  end
+template '/etc/monit/conf.d/sidekiq_conectaideas.monitrc' do
+  owner 'root'
+  group 'root'
+  mode 0644
+  source 'monitrc.conf.erb'
+  variables({
+    :worker_count => 1,
+    :app_name => 'conectaideas',
+    :deploy => node[:deploy][:conectaideas]
+  })
+end
 
-  execute "ensure-sidekiq-is-setup-with-monit" do
-    command %Q{
-      monit reload
-    }
-  end
+execute 'ensure-sidekiq-is-setup-with-monit' do
+  command 'monit reload'
+end
 
-  execute "restart-sidekiq" do
-    command %Q{
-      echo "sleep 20 && monit -g sidekiq_#{application} restart all" | at now
-    }
-  end
+execute 'restart-sidekiq' do
+  command 'sleep 20 && monit -g sidekiq_conectaideas restart all | at now'
 end
 
 cron_d 'cerrar-sesiones-abiertas' do
