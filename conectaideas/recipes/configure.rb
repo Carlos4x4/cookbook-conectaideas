29e2361834ef1b2ed9409c0bb08ebc05cae6e883
Manuel Bahamondez Honores <manuel@bahamondez.com>
2014-05-05 09:17:49 -0400

    create config folder if it doesn't exist

--- conectaideas/recipes/configure.rb
+++
@@ -1,9 +0,0 @@
-node[:deploy].keys.each do |app|
-  template "#{node[:deploy][app][:deploy_to]}/shared/config/application.yml" do
-    owner 'deploy'
-    group 'deploy'
-    mode 0750
-    source 'application.yml.erb'
-    variables({:env => node[:deploy][app][:environment]})
-  end
-end
--- conectaideas/recipes/default.rb
+++ conectaideas/recipes/default.rb
@@ -106,3 +106,16 @@ end
 #   interpreter 'bash'
 #   code 'swapon -a'
 # end
+
+node[:deploy].keys.each do |app|
+  execute 'mkdir' do
+    command "mkdir -p #{node[:deploy][app][:deploy_to]}/shared/config/"
+  end
+  template "#{node[:deploy][app][:deploy_to]}/shared/config/application.yml" do
+    owner 'deploy'
+    group 'deploy'
+    mode 0750
+    source 'application.yml.erb'
+    variables({:env => node[:deploy][app][:environment]})
+  end
+end
