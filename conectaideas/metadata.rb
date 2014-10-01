ca1ec44435fc67da8cbc27a44419e75584c78eb7
Manuel Bahamondez Honores <manuel@bahamondez.com>
2014-10-01 09:01:40 -0300

    version

--- conectaideas/metadata.rb
+++ conectaideas/metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'manuel@bahamondez.com'
 license          'All rights reserved'
 description      'Installs/Configures conectaideas'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
