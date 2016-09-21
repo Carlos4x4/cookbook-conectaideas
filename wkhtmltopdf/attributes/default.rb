0924f673e28cac6574c9c3dde2b101b6ce682865
Carlos <carlosaguirre@automind.cl>
2016-09-21 08:56:02 -0300

    arreglo direcciones librería

--- wkhtmltopdf/attributes/default.rb
+++ wkhtmltopdf/attributes/default.rb
@@ -5,6 +5,7 @@ else
 end
 default['wkhtmltopdf']['install_dir'] = "/usr/local/bin"
 default['wkhtmltopdf']['mirror_url'] = "http://download.gna.org/wkhtmltopdf/0.12/0.12.2.1/wkhtmltox-0.12.2.1.tar.bz2"
+#default['wkhtmltopdf']['mirror_url'] = "http://download.gna.org/wkhtmltopdf/0.12/0.12.2.1/wkhtmltox-0.12.2.1_linux-trusty-amd64.deb"
 #default['wkhtmltopdf']['version'] = '0.10.0_rc2'
 default['wkhtmltopdf']['version'] = '0.12.2.1'
 
