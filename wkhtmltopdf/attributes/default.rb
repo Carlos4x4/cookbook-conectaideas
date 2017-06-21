69d00eb1df5260837dfcc144124a7a12890a3090
Carlos <carlosaguirre@automind.cl>
2017-06-21 00:28:32 -0400

    colviendo a version 0.10.0_rc2

--- wkhtmltopdf/attributes/default.rb
+++ wkhtmltopdf/attributes/default.rb
@@ -6,17 +6,15 @@ end
 default['wkhtmltopdf']['install_dir'] = "/usr/local/bin"
 #default['wkhtmltopdf']['mirror_url'] = "http://wkhtmltopdf.googlecode.com/files"
 # default['wkhtmltopdf']['mirror_url'] = "http://download.gna.org/wkhtmltopdf/obsolete/linux"
-default['wkhtmltopdf']['mirror_url'] = "https://downloads.wkhtmltopdf.org/obsolete/linux"
+default['wkhtmltopdf']['mirror_url'] = "https://downloads.wkhtmltopdf.org/obsolete/linux/"
 #default['wkhtmltopdf']['mirror_url'] = "http://download.gna.org/wkhtmltopdf/0.12/0.12.2.1"
-default['wkhtmltopdf']['version'] = '0.9.9'
+default['wkhtmltopdf']['version'] = '0.10.0_rc2'
 #default['wkhtmltopdf']['version'] = '0.12.2.1'
 
-
-
 # wkhtmltoimage
-# default['wkhtmltopdf']['wkhtmltoimage']['binary_extracted_name'] = "wkhtmltoimage-#{node['wkhtmltopdf']['arch']}"
-# default['wkhtmltopdf']['wkhtmltoimage']['binary_full_name'] = "wkhtmltoimage-#{node['wkhtmltopdf']['version']}-static-#{node['wkhtmltopdf']['arch']}"
-# default['wkhtmltopdf']['wkhtmltoimage']['binary_url'] = "#{node['wkhtmltopdf']['mirror_url']}/#{node['wkhtmltopdf']['wkhtmltoimage']['binary_full_name']}.tar.bz2"
+default['wkhtmltopdf']['wkhtmltoimage']['binary_extracted_name'] = "wkhtmltoimage-#{node['wkhtmltopdf']['arch']}"
+default['wkhtmltopdf']['wkhtmltoimage']['binary_full_name'] = "wkhtmltoimage-#{node['wkhtmltopdf']['version']}-static-#{node['wkhtmltopdf']['arch']}"
+default['wkhtmltopdf']['wkhtmltoimage']['binary_url'] = "#{node['wkhtmltopdf']['mirror_url']}/#{node['wkhtmltopdf']['wkhtmltoimage']['binary_full_name']}.tar.bz2"
 
 # wkhtmltopdf
 default['wkhtmltopdf']['wkhtmltopdf']['binary_extracted_name'] = "wkhtmltopdf-#{node['wkhtmltopdf']['arch']}"
